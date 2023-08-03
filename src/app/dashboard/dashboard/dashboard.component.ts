a74a380c996d14605ce6bf7f0c372d2d6ee5a0e8
Romanko2 <kholoshenkor@gmail.com>
2023-08-03 17:43:41 +0530

    changes

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -12,7 +12,7 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  timeStamp = new FormControl()
+  timeStamp = new FormControl('end')
   selectVal:any;
  ngOnInit(): void {
    
