7c9e6e1063d22827aed3daf0191e00330cc06c2b
Romanko2 <you@example.com>
2023-08-03 10:25:00 +0530

    am-chart

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,18 +4,23 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeatureModule } from '../feature/feature.module';
+import { StockChartComponent } from './stock-chart/stock-chart.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    StockChartComponent,
 
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    FeatureModule
+    FeatureModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
