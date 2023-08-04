59064231bd5e3c0aa8b3fe1de6cdbd54519a506b
Romanko2 <you@example.com>
2023-08-04 17:33:25 +0530

    fixes

--- src/app/dashboard/stock-chart/stock-chart.component.ts
+++ src/app/dashboard/stock-chart/stock-chart.component.ts
@@ -26,52 +26,80 @@ export class StockChartComponent implements OnInit {
     this.getIntradaygraph()
   }
 
+  subtractOneDay(dateString: string): string {
+    const parts = dateString.split('-');
+    const year = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1; // Month is zero-based (0-11)
+    const day = parseInt(parts[2], 10);
+
+    const date = new Date(year, month, day);
+    date.setDate(date.getDate() - 1); // Subtract 1 day
+
+    const resultYear = date.getFullYear();
+    const resultMonth = date.getMonth() + 1; // Months are zero-based, so add 1
+    const resultDay = date.getDate();
+
+    // Format the result in the 'yyy-dd-mm' format
+    const formattedResult = `${resultYear}-${resultMonth.toString().padStart(2, '0')}-${resultDay.toString().padStart(2, '0')}`;
+    return formattedResult;
+  }
+
+
+  formatDateToCustomFormat(dateString: any): any {
+    const date = new Date(dateString);
+    const options: any = { day: '2-digit', month: 'short' };
+    return date.toLocaleDateString('en', options);
+  }
+
+
   getIntradaygraph() {
     let data = {
       symbol: 'AAPL',
       // limit: 30,
       // offset: 30,
-     
+
     }
     this.bs.load(true)
     this.fs.getgraph('End_of_Day', data).subscribe({
       next: (res: any) => {
         this.bs.load(false)
         this.perDayGraphData = res.data.data
-        const perDayGraphData = res.data.data
         let tDate: any;
         let datess: any[] = []
         let highs: any[] = []
         let closee: any[] = []
         let loww: any[] = []
-        perDayGraphData.forEach((res: any) => {
-          datess.push(new Date(res.date).toLocaleDateString('en-us'))
+
+
+        this.perDayGraphData.forEach((res: any) => {
+
+          // datess.push(new Date(res.date).toLocaleDateString('en'))
+          datess.push(this.formatDateToCustomFormat(res.date))
           highs.push(res.high)
           closee.push(res.close)
           loww.push(res.low)
 
         })
         this.perDayGraphData.forEach((x: any) => {
+
           const dateTime = x.date.split('T');
           x.dateOnly = dateTime[0];
           let newDate = x.dateOnly
-          console.log(newDate)
           var ctx = document.getElementById('eod') as HTMLCanvasElement
           this.eodChart = new Chart(ctx, {
             type: 'line', //this denotes tha type of chart
             data: {
               labels: datess,
-
               datasets: [
                 {
-                  label: 'High Prices',
+                  // label: 'High Prices',
                   data: highs,
                   borderColor: '#80ed99',
 
                   borderWidth: 2,
                   fill: false
                 },
-       
+
               ]
             },
             options: this.stockChartOptions
@@ -90,6 +118,11 @@ export class StockChartComponent implements OnInit {
     responsive: true,
     maintainAspectRation: false,
 
+    plugins: {
+      legend: {
+        display: false // Hide the legend
+      }
+    },
     scales: {
       x: {
         grid: {
@@ -105,4 +138,17 @@ export class StockChartComponent implements OnInit {
     }
   }
 
+
+
+  filterDataByTimeRange(days: number): any[] {
+    // Filter data based on the selected time range
+    const endDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(startDate.getDate() - days);
+    return this.perDayGraphData.filter((item) => {
+      console.log(item.date)
+      const date = new Date(item.date);
+      return date >= startDate && date <= endDate;
+    });
+  }
 }
